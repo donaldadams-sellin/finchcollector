dab7748894960e45512d841a036df389f36d4bc1
Donald Adams-Sellin <snave1993@hotmail.com>
2022-01-10 22:03:32 -0500

    Add template files, add Finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=25)
+    favorite_music = models.CharField(max_length=25)
+    age = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finch_index(request):
+    return render(request, 'finches/index.html')
