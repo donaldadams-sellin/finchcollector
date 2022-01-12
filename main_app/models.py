1e2711b056abe206c0c7e0c071c9a2ec85e5f6cd
Donald Adams-Sellin <snave1993@hotmail.com>
2022-01-11 22:43:20 -0500

    Add Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=25)
@@ -13,3 +16,23 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse('finches_detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    TIMES = (
+    ('D', 'Day'),
+    ('N', 'Night')
+    )
+    FOODS = (
+        ('S', 'Seeds'),
+        ('I', 'Insect')
+    )
+    date = models.DateField()
+    time = models.CharField(max_length=1, choices=TIMES, default=TIMES[0][0])
+    food = models.CharField(max_length=1, choices=FOODS, default=FOODS[0][0])
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Fed {self.get_food_display()} at {self.get_time_display()} on {self.date}'
+
+    class Meta:
+        ordering = ['-date']
